560c9b51279e57b32dba635cdae851f0ea170140
Matthias Nielsen <mscn@itu.dk>
2025-11-29 23:43:53 +0100

    feat: add AddProductDialog

--- Vault/composeApp/src/commonMain/kotlin/org/creategoodthings/vault/domain/repositories/ProductRepository.kt
+++ Vault/composeApp/src/commonMain/kotlin/org/creategoodthings/vault/domain/repositories/ProductRepository.kt
@@ -3,10 +3,14 @@ package org.creategoodthings.vault.domain.repositories
 import kotlinx.coroutines.flow.Flow
 import org.creategoodthings.vault.data.local.ProductEntity
 import org.creategoodthings.vault.data.local.StorageEntity
+import org.creategoodthings.vault.domain.Product
+import kotlin.uuid.ExperimentalUuidApi
 
+@OptIn(ExperimentalUuidApi::class)
 interface ProductRepository {
-    suspend fun insertProduct(product: ProductEntity)
-    suspend fun getAllProducts(): Flow<List<ProductEntity>>
-    fun getStorageWithProducts(): Flow<Map<StorageEntity, List<ProductEntity>>>
+    suspend fun insertProduct(product: Product)
+    suspend fun getAllProductsOrderedByAlphabet(): Flow<List<ProductEntity>>
+    fun getStorageWithProductsOrderedByBB(): Flow<Map<StorageEntity, List<ProductEntity>>>
     fun getProductsOrderedByBB(): Flow<List<ProductEntity>>
 }
+
