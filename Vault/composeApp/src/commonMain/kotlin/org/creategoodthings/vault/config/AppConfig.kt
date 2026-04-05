0067c1d78b9219764ea1581e76f4204e912f8716
Matthias Nielsen <matthias.s.nielsen@protonmail.com>
2026-04-05 13:45:02 +0200

    chore: pointed project towards api endpoint

--- Vault/composeApp/src/commonMain/kotlin/org/creategoodthings/vault/config/AppConfig.kt
+++ Vault/composeApp/src/commonMain/kotlin/org/creategoodthings/vault/config/AppConfig.kt
@@ -12,5 +12,5 @@ object AppConfig {
 
     val BASE_URL: String
         get() = if (IS_DEV) "http://10.0.2.2:8090" //Android Platform to reach host IP
-                else "https://api.creategoodthings.dk/vault" //TODO Is this the right endpoint?
+                else "https://vault-api.creategoodthings.dk"
 }
